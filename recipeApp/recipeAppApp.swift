a5f6240346887e1539a8e3b06ae53e53bd05b73d
Josh Brueck <jabrueck@gmail.com>
2025-11-20 14:08:44 -0600

    Initial Commit

---
+++ recipeApp/recipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppApp.swift
+//  recipeApp
+//
+//  Created by Josh Brueck on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct recipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipeAppTests/recipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppTests.swift
+//  recipeAppTests
+//
+//  Created by Josh Brueck on 11/20/25.
+//
+
+import Testing
+@testable import recipeApp
+
+struct recipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
